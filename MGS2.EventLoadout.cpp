f7b17da708e0c17e1e861ac7e42635d3ffc0efe0
onni-forsblom <onni.forsblommi@gmail.com>
2025-01-09 17:22:17 +0200

    Added setting alert mode in event loadout

--- MGS2.EventLoadout.cpp
+++ MGS2.EventLoadout.cpp
@@ -7,6 +7,7 @@
 
 namespace MGS2::EventLoadout {
 	const char* Category = "EventLoadout";
+	static int AlertModeToSet = 255;
 
 	static const std::unordered_map<std::string, Stage> NameToStageMap{
 		{"tanker", Stage::Tanker},
@@ -29,6 +30,13 @@ namespace MGS2::EventLoadout {
 		{"e-extreme", Difficulty::EuroExtreme},
 	};
 
+	static const std::unordered_map<std::string, AlertMode::Enum> NameToAlertModeMap{
+		{"infiltration", AlertMode::Infiltration},
+		{"alert", AlertMode::Alert},
+		{"evasion", AlertMode::Evasion},
+		{"caution", AlertMode::Caution},
+	};
+
 	struct ItemData {
 		std::uint8_t Number = 255;
 		short Amount = -2;
@@ -42,6 +50,7 @@ namespace MGS2::EventLoadout {
 		short EquipmentToEquip = -1;
 		short Progress = -1;
 		std::uint8_t Difficulty = 0;
+		std::uint8_t AlertMode = 255;
 	};
 
 
@@ -129,7 +138,7 @@ namespace MGS2::EventLoadout {
 				(stage == Stage::Tanker
 					|| characterCode == snakeBossSurvivalCharCode);
 
-			// Finally, set all the data for the items, progress and difficulty
+			// Finally, set all the data for the items, progress, difficulty and alert mode
 
 			SetItemsData(itemsDataIt->second.WeaponsData, isTankerOrSnakeBossSurvival);
 			SetItemsData(itemsDataIt->second.EquipmentData, isTankerOrSnakeBossSurvival, false);
@@ -146,8 +155,52 @@ namespace MGS2::EventLoadout {
 			if (itemsDataIt->second.Difficulty != 0) {
 				*Mem::Difficulty = itemsDataIt->second.Difficulty;
 			}
+			AlertModeToSet = itemsDataIt->second.AlertMode;
 
 		catch_mgs2(Category, "884CA0")
+	}
+
+	// Before/after running main GCL for stage
+	// Better to change some data here (like alert mode)
+	tFUN_Int_Int oFUN_004e4090;
+	int hkFUN_004e4090(int param_1) {
+
+		int result = oFUN_004e4090(param_1);
+
+		try_mgs2
+			// 255 indicates to not change the alert mode
+			if (AlertModeToSet == 255) {
+				return result;
+			}
+
+			// The alert mode stored between areas
+			char& storedAlertMode = *(char*)0x118AEDC;
+
+			// If the stored alert mode is more severe than the one we want to set
+			// do not change the alert mode (e.g. do not demote from alert to caution)
+			if (storedAlertMode != AlertMode::Infiltration
+				&& storedAlertMode < AlertModeToSet) {
+				AlertModeToSet = 255;
+				return result;
+			}
+
+			// Set the alert mode that is stored between loads
+			storedAlertMode = AlertModeToSet;
+
+			// If we want to set caution mode,
+			// set the caution time that is stored between loads to 3600 frames (60 s)
+			if (AlertModeToSet == AlertMode::Caution) {
+				int& StoredCautionTime = *(int*)0xF6DE10;
+				StoredCautionTime = 3600;
+			}
+
+			// Do this so as not to change alert mode with the next load
+			// unless we want to
+			AlertModeToSet = 255;
+			
+		catch_mgs2(Category, "4E4090");
+
+		return result;
 	}
 	
 	// For turning a string lowercase
@@ -182,6 +235,8 @@ namespace MGS2::EventLoadout {
 		if (ini.IsEmpty() || (!ini.GetBoolValue(Category, "Enabled", false))) {
 			return;
 		}
+
+		bool needToHookForMainGCL = false;
 
 		// Warning on starting a new game if loadout data was parsed
 		NewGameInfo::AddWarning("Event Loadout", &NewGameInfoCallback);
@@ -236,7 +291,7 @@ namespace MGS2::EventLoadout {
 				std::string value = ToLower(ini.GetValue(section.pItem, sectionKey.pItem));
 				std::string sectionKeyStr = ToLower(sectionKey.pItem);
 
-				// Check if we want to set something other than item data (equipped item[s], progress, difficulty)
+				// Check if we want to set something other than item data (equipped item[s], progress, difficulty, alert mode)
 				// and act accordingly
 				const std::set<std::string> weaponEquipStrings = {"weapon", "wpn"};
 				if (weaponEquipStrings.contains(sectionKeyStr)) {
@@ -260,6 +315,15 @@ namespace MGS2::EventLoadout {
 				const std::set<std::string> difficultyStrings = {"difficulty", "diff"};
 				if (difficultyStrings.contains(sectionKeyStr)) {
 					loadoutData.Difficulty = NameToDifficultyMap.find(value)->second;
+					continue;
+				}
+				const static std::set<std::string> alertLevelStrings = {"alertmode", "alertlevel"};
+				if (alertLevelStrings.contains(sectionKeyStr)) {
+					needToHookForMainGCL = true;
+					if (TrySetNumFromStr(loadoutData.AlertMode, value)) {
+						continue;
+					}
+					loadoutData.AlertMode = NameToAlertModeMap.find(value)->second;
 					continue;
 				}
 
@@ -311,5 +375,11 @@ namespace MGS2::EventLoadout {
 
 		// Hook function that activates on load
 		oFUN_00884ca0 = (tFUN_Void)mem::TrampHook32((BYTE*)0x884CA0, (BYTE*)hkFUN_00884ca0, 6);
+
+		// Hook function that activates before/after running main GCL for stage
+		// but only if there is a need
+		if (needToHookForMainGCL) {
+			oFUN_004e4090 = (tFUN_Int_Int)mem::TrampHook32((BYTE*)0x4E4090, (BYTE*)hkFUN_004e4090, 8);
+		}
 	}
 }
